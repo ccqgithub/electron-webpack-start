4ce1225d54b067d6f7b20450fffd4820a05d7329
season.chen <season.chen.i@foxmail.com>
2019-03-12 16:09:25 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const amdLoader = require('monaco-editor/min/vs/loader');
+const amdRequire = amdLoader.require;
+// const amdDefine = amdLoader.require.define;
+const novel = require('./language.novel');
+
+function uriFromPath(_path) {
+  let pathName = path.resolve(_path).replace(/\\/g, '/');
+  if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+    pathName = '/' + pathName;
+  }
+  return encodeURI('file://' + pathName);
+}
+
+// workaround monaco-css not understanding the environment
+self.module = undefined;
+amdRequire.config({
+  baseUrl: uriFromPath(path.join(__dirname, '../node_modules/monaco-editor/min'))
+});
+
+amdRequire(['vs/editor/editor.main'], function(monaco) {
+  novel.registerLanguage(monaco);
+  const editor = monaco.editor.create(document.getElementById('container'), {
+    theme: 'myCoolTheme',
+    value: [
+      'hello world!',
+    ].join('\n'),
+    language: 'novel'
+  });
+});
---
+++ src/language.novel.js
@@ -0,0 +1,51 @@
+exports.registerLanguage = function(monaco) {
+  // Register a new language
+  monaco.languages.register({ id: 'novel' });
+
+  // Register a tokens provider for the language
+  monaco.languages.setMonarchTokensProvider('novel', {
+    tokenizer: {
+      root: [
+        [/hello/, "role"],
+      ]
+    }
+  });
+
+  // Define a new theme that contains only rules that match this language
+  monaco.editor.defineTheme('myCoolTheme', {
+    base: 'vs',
+    inherit: false,
+    rules: [
+      { token: 'role', foreground: 'ff0000', fontStyle: 'bold' },
+    ]
+  });
+
+  // Register a completion item provider for the new language
+  monaco.languages.registerCompletionItemProvider('mySpecialLanguage', {
+    provideCompletionItems: () => {
+      var suggestions = [{
+        label: 'simpleText',
+        kind: monaco.languages.CompletionItemKind.Text,
+        insertText: 'simpleText'
+      }, {
+        label: 'testing',
+        kind: monaco.languages.CompletionItemKind.Keyword,
+        insertText: 'testing(${1:condition})',
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+      }, {
+        label: 'ifelse',
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: [
+          'if (${1:condition}) {',
+          '\t$0',
+          '} else {',
+          '\t',
+          '}'
+        ].join('\n'),
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        documentation: 'If-Else Statement'
+      }];
+      return { suggestions: suggestions };
+    }
+  });
+}
