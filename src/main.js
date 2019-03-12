3918d3a88a19d604ca190e46150dd0c59c392916
season.chen <season.chen.i@foxmail.com>
2019-03-12 16:11:29 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
-let win
+let win;
 
-function createWindow () {
+function createWindow() {
   // 创建浏览器窗口。
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 800, height: 600 });
 
   // 然后加载应用的 index.html。
-  win.loadFile('src/test.html')
+  win.loadFile(path.resolve(__dirname, './test.html'));
 
   // 打开开发者工具
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
 
   // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
@@ -21,13 +21,13 @@ function createWindow () {
     // 通常会把多个 window 对象存放在一个数组里面，
     // 与此同时，你应该删除相应的元素。
     win = null
-  })
+  });
 }
 
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
@@ -36,15 +36,15 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   // 在macOS上，当单击dock图标并且没有其他窗口打开时，
   // 通常在应用程序中重新创建一个窗口。
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // 在这个文件中，你可以续写应用剩下主进程代码。
 // 也可以拆分成几个文件，然后用 require 导入。
